11f2f8b98611f6093343f16a84a0255d6f2afa03
zulfiyyahuseynova <tu7kswf4d@code.edu.az>
2022-03-15 05:54:39 -0700

    added

---
+++ WeaponClass/Model/Weapon.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeaponClass.Model
+{
+    class Weapon
+    {
+        public int bulletCapacity;
+        public int currentBullet;
+        public double dischargeSecond;
+        public bool shootMode;
+
+        public Weapon(int bulletCapacity, int currentBullet, double dischargeSecond, bool shootMode)
+        {
+            this.bulletCapacity = bulletCapacity;
+            this.currentBullet = currentBullet;
+            this.dischargeSecond = dischargeSecond;
+            this.shootMode = shootMode;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("1-Shoot metodu üçün \n" + "2-Fire metodu üçün \n" + "3-GetRemainBulletCount metodu üçün \n" + "4-Reload metodu üçün \n" + "5-ChangeFireMode üçün \n" +
+                "6-Proqramı dayandırmaq üçün qısayoldur");
+        }
+        public void Shoot()
+        {
+            if (currentBullet == 0)
+            {
+                Console.WriteLine("Daraqda güllə yoxdur");
+            }
+            else if(currentBullet > 0)
+            {
+                currentBullet -= 1;
+                Console.WriteLine("1 güllə atıldı");
+            }
+            else
+            {
+                Console.WriteLine("Güllə sayı mənfi ola bilməz");
+            }            
+        }
+        public void Fire()
+        {
+            if(currentBullet < bulletCapacity && currentBullet>=0 && dischargeSecond > 0 && bulletCapacity > 0)
+            {
+                double second = (currentBullet * dischargeSecond) / bulletCapacity;
+                Console.WriteLine($"{second} saniyəyə bitir");
+            }
+            else
+            {
+                Console.WriteLine("Qiymətləri düzgün daxil edin");
+            }
+            
+        }
+        public void GetRemainBulletCount()
+        {
+            if (bulletCapacity > 0 && currentBullet >= 0 && currentBullet < bulletCapacity) 
+            { 
+            int neededBulletCount = bulletCapacity - currentBullet;
+            Console.WriteLine($"Darağın tam dolması üçün {neededBulletCount} güllə lazımdır");
+            }
+            else
+            {
+                Console.WriteLine("Qiymətləri düzgün daxil edin");
+            }
+
+        }
+        public void Reload()
+        {
+            if (bulletCapacity > 0 && currentBullet >= 0 && currentBullet < bulletCapacity)
+            {
+                int neededBulletCount = bulletCapacity - currentBullet;
+                currentBullet += neededBulletCount;
+                Console.WriteLine("Daraq yenidən dolduruldu"); 
+            }
+            else
+            {
+                Console.WriteLine("Qiymətləri düzgün daxil edin");
+            }
+        }
+        public void changeFireMode()
+        {
+            if (shootMode == true)
+            {
+                shootMode = false;
+                Console.WriteLine("Atış modu avtomatik olaraq dəyişdirildi");
+            }
+            else if (shootMode == false)
+            {
+                shootMode = true;
+                Console.WriteLine("Atış modu təkli olaraq dəyişdirildi");
+            }
+        }
+    }
+}
---
+++ WeaponClass/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using WeaponClass.Model;
+
+namespace WeaponClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Darağın güllə tutumunu daxil edin:");
+            int bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Daraqdakı güllə sayını daxil edin:");
+            int currentBullet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Darağın boşalma saniyəsini daxil edin:");
+            double dischargeSecond = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Atış modunu daxil edin:");
+            bool shootMode = Convert.ToBoolean(Console.ReadLine());
+            Weapon gun = new Weapon(bulletCapacity,currentBullet,dischargeSecond,shootMode);
+            Console.WriteLine("İnformasiya almaq üçün 0 seçin");
+
+            int key = 0;
+            do
+            {
+                Console.WriteLine("Rəqəm daxil edin:");
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 0:
+                        gun.ShowInfo();
+                        break;
+                    case 1:
+                        gun.Shoot();
+                        break;
+                    case 2:
+                        gun.Fire();
+                        break;
+                    case 3:
+                        gun.GetRemainBulletCount();
+                        break;
+                    case 4:
+                        gun.Reload();
+                        break;
+                    case 5:
+                        gun.changeFireMode();
+                        break;
+                    case 6:
+                        Console.WriteLine("Proqram dayandırıldı");
+                        break;
+                    default:
+                        Console.WriteLine("Belə bir metod yoxdur");
+                        break;
+                }
+
+            } while (key != 6);
+        }
+    }
+}
